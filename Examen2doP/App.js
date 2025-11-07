3d7c48fed8c0e65129d29d06131af7b02101dab4
121039673-boop <121039673@upq.edu.mx>
2025-11-07 15:30:59 -0600

    Examen

--- Examen2doP/App.js
+++ Examen2doP/App.js
@@ -1,8 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, ImageBackground, Button, SafeAreaView } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground, Button, SafeAreaView, TextInput, Alert, ScrollView, RefreshControl } from 'react-native';
 
 export default function App() {
-    const [showSplash, setShowSplash] = useState(true);
+  const [showSplash, setShowSplash] = useState(true);
+
+  const mostrarAlerta = () => {
+    <Button
+      title="Guardar"
+      onPress={mostrarAlerta}
+      color="#2377caff" />
+  }
+
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 1000); 
+  };
+
+
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -17,80 +35,119 @@ export default function App() {
     return (
       <View style={styles.splashContainer}>
         <Text style={styles.splashText}>Bienvenido a la App</Text>
-       </View>
-   );
-   }
-  
+      </View>)
+  }
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      
-       <ImageBackground
+
+      <ImageBackground
         source={{
           uri: 'https://tse3.mm.bing.net/th/id/OIP.bpEdKZYhh8c9MLYQvNqBswHaEo?rs=1&pid=ImgDetMain&o=7&rm=3',
-        }} 
+        }}
         style={styles.background} // se aplican los estilos del fondo
       >
-        {/* View que contiene el texto y el botón, con un fondo semitransparente */}
         <View style={styles.overlay}>
           {/* Texto principal */}
           <Text style={styles.title}>Valeria Briones Patiño</Text>
+          <Text style={styles.title}>Valeria Briones Patiño</Text>
+          <Text style={styles.title}>Valeria Briones Patiño</Text>
+          <Text style={styles.title}>Valeria Briones Patiño</Text>
+          <Text style={styles.title}>Valeria Briones Patiño</Text>
+          <Text style={styles.title}>Valeria Briones Patiño</Text>
+          <Text style={styles.title}>Valeria Briones Patiño</Text>
+          <Text style={styles.title}>Valeria Briones Patiño</Text>
+          <Text style={styles.title}>Valeria Briones Patiño</Text>
+          <Text style={styles.title}>Valeria Briones Patiño</Text>
+          <Text style={styles.title}>Valeria Briones Patiño</Text>
+          <Text style={styles.title}>Valeria Briones Patiño</Text>
+          <Text style={styles.title}>Valeria Briones Patiño</Text>
+          <Text style={styles.title}>Valeria Briones Patiño</Text>
+          <Text style={styles.title}>Valeria Briones Patiño</Text>
+          <Text style={styles.title}>Valeria Briones Patiño</Text>
+          <Text style={styles.title}>Valeria Briones Patiño</Text>
+          <Text style={styles.title}>Valeria Briones Patiño</Text>
+          <Text style={styles.title}>Valeria Briones Patiño</Text>
+          <Text style={styles.title}>Valeria Briones Patiño</Text>
+          <Text style={styles.title}>Valeria Briones Patiño</Text>
+          <Text style={styles.title}>Valeria Briones Patiño</Text>
+          <Text style={styles.title}>Valeria Briones Patiño</Text>
+          <Text style={styles.title}>Valeria Briones Patiño</Text>
+          <Text style={styles.title}>Valeria Briones Patiño</Text>
+
 
           <Text style={styles.title}>Estudiante de TIID</Text>
 
-
           <Text style={styles.title}>Soy estudiante de la carrera de TIID, y me apasiona el desarrollo web y
-             las bases de datos.</Text>
+            las bases de datos.</Text>
 
           <Text style={styles.title}>Correo: 121039673@upq.edu.mx</Text>
 
           <Text style={styles.title}>Teléfono: 4425012541</Text>
 
+          <Button
+            title="Editar Perfil"
+            onPress={mostrarAlerta}
+            color="#2377caff" />
+
+          <ScrollView
+            showsVerticalScrollIndicator={false}
+            refreshControl={
+              <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+            }>
+              
+            stickyHeaderIndices={[0]}
+            </ScrollView>
+
+
         </View>
       </ImageBackground>
-    </SafeAreaView>
+    </SafeAreaView >
   );
 }
 
-  
-  
+
+
+
+
 const styles = StyleSheet.create({
- 
+
   splashContainer: {
-    flex: 1, 
-    backgroundColor: '#add9e44b', 
-    justifyContent: 'center', 
-    alignItems: 'center', 
+    flex: 1,
+    backgroundColor: '#add9e44b',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 
-  
+
   splashText: {
-    fontSize: 24, 
-    color: '#3e0bf6ff', 
+    fontSize: 24,
+    color: '#3e0bf6ff',
   },
 
-  
+
   background: {
-    flex: 1, 
-    justifyContent: 'center', 
-    alignItems: 'center', 
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   overlay: {
-    backgroundColor: 'rgba(0,0,0,0.5)', 
-    padding: 20, 
-    borderRadius: 10, 
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    padding: 20,
+    borderRadius: 10,
   },
 
-  
+
   title: {
-    fontSize: 28, 
-    color: '#fff', 
-    marginBottom: 10, 
-    textAlign: 'center', 
+    fontSize: 28,
+    color: '#fff',
+    marginBottom: 10,
+    textAlign: 'center',
   },
 
 });
 
-  
+
 
 
 
