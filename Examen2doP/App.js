ac70f4fadaa6170a60014e583350fefc1d0d6a79
121039673-boop <121039673@upq.edu.mx>
2025-11-07 14:50:14 -0600

    Primer commit Examen

--- Examen2doP/App.js
+++ Examen2doP/App.js
@@ -1,20 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, ImageBackground, Button, SafeAreaView } from 'react-native';
 
 export default function App() {
+    const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false); // cambia el estado para ocultar el splash
+    }, 4000);
+    return () => clearTimeout(timer); // limpia el temporizador al desmontar el componente
+  }, []);
+
+  // Si "showSplash" es verdadero, se muestra esta vista (pantalla de bienvenida) 
+  if (showSplash) {
+
+    return (
+      <View style={styles.splashContainer}>
+        <Text style={styles.splashText}>Bienvenido a la App</Text>
+       </View>
+   );
+   }
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      
+       <ImageBackground
+        source={{
+          uri: 'https://tse3.mm.bing.net/th/id/OIP.bpEdKZYhh8c9MLYQvNqBswHaEo?rs=1&pid=ImgDetMain&o=7&rm=3',
+        }} 
+        style={styles.background} // se aplican los estilos del fondo
+      >
+        {/* View que contiene el texto y el botón, con un fondo semitransparente */}
+        <View style={styles.overlay}>
+          {/* Texto principal */}
+          <Text style={styles.title}>Valeria Briones Patiño</Text>
+
+          <Text style={styles.title}>Estudiante de TIID</Text>
+
+
+          <Text style={styles.title}>Soy estudiante de la carrera de TIID, y me apasiona el desarrollo web y
+             las bases de datos.</Text>
+
+          <Text style={styles.title}>Correo: 121039673@upq.edu.mx</Text>
+
+          <Text style={styles.title}>Teléfono: 4425012541</Text>
+
+        </View>
+      </ImageBackground>
+    </SafeAreaView>
   );
 }
 
+  
+  
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+ 
+  splashContainer: {
+    flex: 1, 
+    backgroundColor: '#add9e44b', 
+    justifyContent: 'center', 
+    alignItems: 'center', 
   },
+
+  
+  splashText: {
+    fontSize: 24, 
+    color: '#3e0bf6ff', 
+  },
+
+  
+  background: {
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+  },
+  overlay: {
+    backgroundColor: 'rgba(0,0,0,0.5)', 
+    padding: 20, 
+    borderRadius: 10, 
+  },
+
+  
+  title: {
+    fontSize: 28, 
+    color: '#fff', 
+    marginBottom: 10, 
+    textAlign: 'center', 
+  },
+
 });
+
+  
+
+
+
+
+
+
+
+
+
+
+
